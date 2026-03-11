488da2afa3d0e0cf744eb10de5f0adbbb6842532
CLEMS3 <56449459+CLEMS3@users.noreply.github.com>
2026-03-10 22:03:30 -0500

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  // Your repository name on GitHub
+  basePath: '/Personal-website',
+  // Disable server-side image optimization, required for GitHub Pages
+  images: {
+    unoptimized: true,
+  },
+};
 export default nextConfig;
